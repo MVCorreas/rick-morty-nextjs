05328f588e24a8ddbbb045546e1ae432596e23ca
MVCorreas <correasmv@gmail.com>
2025-07-30 17:55:19 -0300

    feat: added basic header and footer displays

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} h-screen flex flex-col`}
       >
-        {children}
+        <header className="items-center justify-between w-full bg-gray-200 p-4">
+          <h1 className="text-2xl font-bold">Header</h1>
+        </header>
+        <main className="flex-1 overflow-auto">{children}</main>
+        <footer className="bg-neutral-700 text-white py-4">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
+            <h1>FOOTER</h1>{" "}
+          </div>
+        </footer>
       </body>
     </html>
   );
