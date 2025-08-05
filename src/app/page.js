75795b3bd86f8b1ec5e934d5dfa66008700f2a1a
MVCorreas <correasmv@gmail.com>
2025-08-05 10:49:05 -0300

    build: installed tailwindscss

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-     <h1>RICK AND MORTY</h1>
-  
+      <h1>RICK AND MORTY</h1>
+      <Link href="/characters">
+        <Button text="View Characters" variant="primary" size="small" />
+      </Link>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'colour-primary': '#3B3B3B',
+        'colour-secondary': '#585660',
+        'colour-tertiary': '#737373',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
